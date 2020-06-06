3ffde4616d8cc5cb8f92af06907a23473bf5a399
Sami Alves <scalves00@gmail.com>
2020-06-06 12:44:18 +0000

    Change to the URL object.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -53,13 +53,11 @@ function bubbleFall() {
  * @param {ArrayList<String>} comments
  */
 function getComments() {
-  const baseURL = '/data';
-  let params = (new URL('https://' + location.hostname + baseURL)).searchParams;
-  const userNumComments = document.getElementById('number-of-comments').value;
-  params.append('number-of-comments', userNumComments);
+    let url = new URL('/data', location.protocol + '//' + location.hostname);
+    const userNumComments = document.getElementById('number-of-comments').value;
+    url.searchParams.append('number-of-comments', userNumComments);
 
-  console.log(params.toString());
-  fetch(baseURL + '?' + params.toString()).then(response => response.json()).then((comments) => {
+    fetch(url).then(response => response.json()).then(comments => {
     console.log(comments);
 
     const commentSectionContainer = document.getElementById('comment-section-container');
