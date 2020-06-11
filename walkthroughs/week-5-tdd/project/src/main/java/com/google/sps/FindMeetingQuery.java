c3805a40c2c93d6a2d848d4a95d6ce6f130197e8
Sami Alves <scalves00@gmail.com>
2020-06-11 15:34:36 +0000

    Write initial ugly/inefficient query function.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,43 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.*;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> results = new ArrayList<TimeRange>();
+
+    // Find conflicts.
+    ArrayList<TimeRange> conflicts = new ArrayList<TimeRange>();
+    for (Event event : events) {
+      if (!Collections.disjoint(event.getAttendees(), request.getAttendees())) {
+        conflicts.add(event.getWhen());
+      }
+    }
+    Collections.sort(conflicts, TimeRange.ORDER_BY_START);
+    Iterator<TimeRange> conflictIterator = conflicts.iterator();
+
+    // Find possible meeting times.
+    long duration = request.getDuration();
+    long currentTime = Long.valueOf(TimeRange.START_OF_DAY);
+    TimeRange previous = TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0);
+    while (conflictIterator.hasNext()) {
+      TimeRange conflictTime = conflictIterator.next();
+      if (previous.contains(conflictTime)) {
+        continue;
+      }
+      long start = Long.valueOf(conflictTime.start());
+      if ((start > currentTime) && ((Long.valueOf(start) - currentTime) >= duration)) {
+        results.add(TimeRange.fromStartEnd((int)currentTime, (int)start, false));
+      }
+      currentTime = conflictTime.end();
+      previous = conflictTime;
+    }
+    if ((Long.valueOf(TimeRange.END_OF_DAY) - currentTime) >= duration) {
+      results.add(TimeRange.fromStartEnd((int)currentTime, TimeRange.END_OF_DAY, true));
+    }
+    return results;
   }
 }
