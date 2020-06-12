da0e7d5e38bc6705f81b6732461736b3a358381b
Sami Alves <scalves00@gmail.com>
2020-06-12 14:15:28 +0000

    Minor edits.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,16 +14,39 @@
 
 package com.google.sps;
 
+import java.lang.Math;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.ArrayList;
-import java.util.*;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
+  /* Identify conflicting events, then iterating through the day, skipping conflicts, and 
+  identifying time ranges longer than the duration of the requested meeting. */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     ArrayList<TimeRange> results = new ArrayList<TimeRange>();
 
-    // Find conflicts.
+    ArrayList<TimeRange> conflicts = findConflicts(events, request);
+    Iterator<TimeRange> conflictIterator = conflicts.iterator();
+    int duration = (int) request.getDuration();
+    int currentTime = TimeRange.START_OF_DAY;
+
+    while (conflictIterator.hasNext()) {
+      TimeRange conflictTime = conflictIterator.next();
+      int start = conflictTime.start();
+      if ((start - currentTime) >= duration) {
+        results.add(TimeRange.fromStartEnd(currentTime, start, false));
+      }
+      currentTime = Math.max(currentTime, conflictTime.end());
+    }
+
+    if ((TimeRange.END_OF_DAY - currentTime) >= duration) {
+      results.add(TimeRange.fromStartEnd(currentTime, TimeRange.END_OF_DAY, true));
+    }
+    return results;
+  }
+
+   private ArrayList<TimeRange> findConflicts(Collection<Event> events, MeetingRequest request) {
     ArrayList<TimeRange> conflicts = new ArrayList<TimeRange>();
     for (Event event : events) {
       if (!Collections.disjoint(event.getAttendees(), request.getAttendees())) {
@@ -31,27 +54,6 @@ public final class FindMeetingQuery {
       }
     }
     Collections.sort(conflicts, TimeRange.ORDER_BY_START);
-    Iterator<TimeRange> conflictIterator = conflicts.iterator();
-
-    // Find possible meeting times.
-    long duration = request.getDuration();
-    long currentTime = Long.valueOf(TimeRange.START_OF_DAY);
-    TimeRange previous = TimeRange.fromStartDuration(TimeRange.START_OF_DAY, 0);
-    while (conflictIterator.hasNext()) {
-      TimeRange conflictTime = conflictIterator.next();
-      if (previous.contains(conflictTime)) {
-        continue;
-      }
-      long start = Long.valueOf(conflictTime.start());
-      if ((start > currentTime) && ((start - currentTime) >= duration)) {
-        results.add(TimeRange.fromStartEnd((int)currentTime, (int)start, false));
-      }
-      currentTime = conflictTime.end();
-      previous = conflictTime;
-    }
-    if ((TimeRange.END_OF_DAY - currentTime) >= duration) {
-      results.add(TimeRange.fromStartEnd((int)currentTime, TimeRange.END_OF_DAY, true));
-    }
-    return results;
-  }
+    return conflicts;
+   }
 }
