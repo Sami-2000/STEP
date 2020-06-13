cc7f2f1db8eac99a662256e41e1c01238d53ea44
Sami Alves <scalves00@gmail.com>
2020-06-13 23:25:35 +0000

    First stab at optimization using BFS. Add 1 test to check for optimizatio (more to come).

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -17,8 +17,10 @@ package com.google.sps;
 import java.lang.Math;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Queue;
 
 public final class FindMeetingQuery {
   /* If we can accomodate optional attendees, do so. Otherwise if there are required attendees,
@@ -33,11 +35,46 @@ public final class FindMeetingQuery {
     Collection<TimeRange> optResults = queryInternal(events, requestWithOptionals);
     
     if ((optResults.isEmpty()) && (!request.getAttendees().isEmpty())) {
-      return queryInternal(events, request);
+      return queryBFS(events, requestWithOptionals, request.getAttendees(),
+          request.getOptionalAttendees());
     }
     else {
       return optResults;
     }
+  }
+
+  /* Follow a BFS, removing optional attendees gradually and returning when you find a timerange
+  which accomodates the remaining optional attendees and the required attendees. If you never
+  find such a timerange, return the results just accounting for required attendees. */
+  public Collection<TimeRange> queryBFS(Collection<Event> events, MeetingRequest request, Collection<String> reqAttendees,
+        Collection<String> optAttendees) {
+    Queue<Collection<String>> queue = new ArrayDeque<Collection<String>>();
+    queue.add(optAttendees);
+
+    while (!queue.isEmpty()) {
+      Collection<String> curOptAttendees = queue.remove();        
+      Collection<String> curAttendees = new ArrayList<String>();
+      curAttendees.addAll(curOptAttendees);
+      curAttendees.addAll(reqAttendees);
+
+      MeetingRequest curRequest = new MeetingRequest(curAttendees, 
+          request.getDuration());
+      Collection<TimeRange> results = queryInternal(events, curRequest);
+      if (!results.isEmpty()) {
+        return results;
+      }
+      
+      else {
+        for (String optAttendee : curOptAttendees) {
+          Collection<String> newOptAttendees = new ArrayList<String>();
+          newOptAttendees.addAll(curOptAttendees);
+          newOptAttendees.remove(optAttendee);
+          queue.add(newOptAttendees);
+        }
+      }
+    }
+    MeetingRequest reqRequest = new MeetingRequest(reqAttendees, request.getDuration());
+    return queryInternal(events, reqRequest);
   }
 
   /* Identify conflicting events, then iterating through the day, skipping conflicts, and 
